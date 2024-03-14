59bf76ac82a0193d4bfa5cc1e139e52e78b45490
Fahad <FFaruqi@dot.nyc.gov>
2024-03-14 15:56:54 -0400

    seperated create and load into functions that call based on the # collections avaliable

--- init_chroma.py
+++ init_chroma.py
@@ -8,13 +8,23 @@ from chromadb.utils import embedding_functions
 
 # experiment with larger models 
 MODEL_NAME = "mixedbread-ai/mxbai-embed-large-v1" # ~ 0.5 gb
+COLLECTION_NAME = "academic_weapon"
 embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=MODEL_NAME)
 
 # Create Chroma Client
 client = chromadb.PersistentClient(path="./chromadb/")
 print(f'Number of active dbs: {client.count_collections()}')
 
-# Create DB
-academic_weapon_db = client.create_collection(
-    name="academic_weapon", embedding_function=embedding_func
-)
+def create_collection() -> collection:
+    academic_weapon_db = client.create_collection(
+        name=COLLECTION_NAME, embedding_function=embedding_func
+    )
+    return academic_weapon_db
+
+def load_collection() -> collection:
+    academic_weapon_db = client.create_collection(
+        name=COLLECTION_NAME, embedding_function=embedding_func
+    )
+    return academic_weapon_db
+
+db = load_collection() if (client.count_collections() != 0) else create_collection()
