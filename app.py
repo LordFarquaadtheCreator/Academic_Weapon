4a037a49db6e99bc147b770da5f35970c423b6ad
Fahad <FFaruqi@dot.nyc.gov>
2024-03-14 16:24:44 -0400

    changed app.py to init chroma on load

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from chroma import *
+import chroma
 
 def main():
+    client = get_client()
+    academic_db = create_collection(client)
+    # academic_db = get_collection(client)
+
     None
 
 
