39de138b57f308fe68aff4066a9e8f9c8bc0c8c5
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-25 17:52:45 -0400

    create_db has if name main

--- langchain/vector-store/create_database.py
+++ langchain/vector-store/create_database.py
@@ -5,15 +5,20 @@ from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def create_database():
+    """ creates a database with two texts """
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-if OPENAI_API_KEY is None:
-    raise Exception("Missing OPENAI_API_KEY")
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    if OPENAI_API_KEY is None:
+        raise Exception("Missing OPENAI_API_KEY")
 
-texts = [
-    "This is unrelated to the second text",
-    "This may relate to unsavory acts of violence",
-]
-db = FAISS.from_texts(texts, OpenAIEmbeddings())
-db.save_local("faiss_index")
+    texts = [
+        "This is unrelated to the second text",
+        "This may relate to unsavory acts of violence",
+    ]
+    db = FAISS.from_texts(texts, OpenAIEmbeddings())
+    db.save_local("faiss_index")
+
+if __name__ == "__main__":
+    create_database()
