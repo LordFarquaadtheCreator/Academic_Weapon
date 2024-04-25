5bef7aa7dfac8221c777c9dccdd77cdddae2425c
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-25 12:39:56 -0400

    fixed bugs from previous pushes

--- langchain/vector-store/create_database.py
+++ langchain/vector-store/create_database.py
@@ -1,12 +1,17 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from dotenv import load_env
+from dotenv import load_dotenv
 import os
-load_env()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if OPENAI_API_KEY is None:
     raise Exception("Missing OPENAI_API_KEY")
 
-db = FAISS(embedding_function = OpenAIEmbeddings())
+texts = [
+    "This is unrelated to the second text",
+    "This may relate to unsavory acts of violence",
+]
+db = FAISS.from_texts(texts, OpenAIEmbeddings())
 db.save_local("faiss_index")
--- langchain/vector-store/upload_data_to_db.py
+++ langchain/vector-store/upload_data_to_db.py
@@ -2,20 +2,23 @@ from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from dotenv import load_env
+from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-DATA_PATH = "../data"
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
+DATA_PATH = "data"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if OPENAI_API_KEY is None:
     raise Exception("Missing OPENAI_API_KEY")
 
-db = FAISS.load_local("faiss_index", OpenAIEmbeddings())
+db = FAISS.load_local(
+    "faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True
+)
 
 for entry in os.listdir(DATA_PATH):
-    loader = TextLoader(entry) #must be txt file
+    loader = TextLoader(os.path.join("data", entry))  # must be txt file
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
