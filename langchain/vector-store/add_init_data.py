7c64c122033fbd935c0b5a9fc0fa3a441580658c
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-05 19:41:14 -0400

    moved functions around

--- langchain/vector-store/add_init_data.py
+++ langchain/vector-store/add_init_data.py
@@ -4,54 +4,13 @@ import os
 from loaders import pdf_pages
 
 
-def encode_image(image) -> str:
-    import io
-    import base64
-
-    byte_arr = io.BytesIO()
-    image.save(byte_arr, format="JPEG")
-    encoded_image = base64.b64encode(byte_arr.getvalue()).decode("utf-8")
-    return encoded_image
-
-
-async def image_to_text(image) -> object:
-    from openai import OpenAI
-    import json
-
-    client = OpenAI()
-
-    response = client.chat.completions.create(
-        model="gpt-4-turbo",
-        response_format={"type": "json_object"},
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Transcribe the contents of this image and return a JSON object that contains the text. It must be structured in the following manner: two entries with the following keys: 'content' and 'text'. Content will be a line describing what the content of text will be, and text will be a simple transcription of the image",
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": f"data:image/jpeg;base64;,{image}",
-                            "detail": "high",
-                        },
-                    },
-                ],
-            }
-        ],
-    )
-    return json.loads(response.choices[0].message.content)
-
-
-async def add_data(dir: str, client):
+def add_data_from_data_dir(dir: str):
     # recursive
     dirs = os.listdir(dir)
 
     for path in dirs:
         if os.path.isdir(os.path.join(dir, path)):  # if dir spotted, recurse into it
-            await add_data(os.path.join(dir, path), client)  # recursive call
+            add_data_from_data_dir(os.path.join(dir, path))  # recursive call
 
         if os.path.join(dir, path).endswith(".pdf"):
             print(f"Processing {path}")
@@ -59,6 +18,4 @@ async def add_data(dir: str, client):
 
 
 if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(add_data("data/"))
+    add_data_from_data_dir("data/")
