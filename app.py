087b09f3cb1d1004d86f0714615cdec1100310ca
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-06 11:19:24 -0400

    updated app to quart app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from quart import Quart, request
+from lang import langchain
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
 @app.route("/")
 def hello():
     return "wrong endpoint"
 
-@app.route("/query")
-async def query(query):
+@app.route("/query", methods=["GET"])
+async def query():
+    query = request.args.get("query")
     res = await langchain(query)
     return res
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ lang.py
@@ -0,0 +1,4 @@
+async def langchain(query: str):
+    #TODO: connect with agents
+    #TODO: return streamer
+    return query
