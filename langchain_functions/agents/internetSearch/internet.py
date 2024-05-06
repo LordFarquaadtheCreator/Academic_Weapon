5037c906c94df10eb986ebf511c05d907b91b749
BVasquez07 <bvasq07@gmail.com>
2024-05-06 12:18:32 -0400

    fix requirements.txt file

--- langchain_functions/agents/internetSearch/internet.py
+++ langchain_functions/agents/internetSearch/internet.py
@@ -11,8 +11,10 @@ import json
 from langchain_core.agents import AgentActionMessageLog, AgentFinish
 from langchain.agents.format_scratchpad import format_to_openai_function_messages
 import requests
-from definitions import BING_API_KEY, OPENAI_API_KEY
+# from definitions import BING_API_KEY, OPENAI_API_KEY
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv, find_dotenv, dotenv_values
+import os
 
 """
 TO DO: get request on the sources and use beautiful soup to extract the text and relevant links within the page(look at previous commits for that). returned by the query and prep them to be sent to the DB (make a function for that and put it in another file)
@@ -20,6 +22,11 @@ TO DO: implement DB interaction.
 https://beautiful-soup-4.readthedocs.io/en/latest/
 
 """
+load_dotenv(find_dotenv('.env'))
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+BING_API_KEY = os.getenv('BING_API_KEY')
+
 
 #Constants
 URL = 'https://api.bing.microsoft.com/v7.0/search'
