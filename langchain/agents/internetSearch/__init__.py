d8780a395ff5d015676e0d1f6557172f69697219
BVasquez07 <bvasq07@gmail.com>
2024-05-05 21:27:51 -0400

    untracked test file

--- langchain/agents/internetSearch/__init__.py
+++
@@ -1,5 +0,0 @@
-from .internet import searchOrNot
-
-# get only the necessary functions from the internet.py file
-
-print('Fetching necessary functions....\n\n')
--- test.py
+++
@@ -1,7 +0,0 @@
-from langChain.agents.internetSearch import searchOrNot
-import time
-
-start = time.time()
-print(searchOrNot('What is the weather like today in Brooklyn? Tell me in F'))
-end = time.time() - start
-print(end)
