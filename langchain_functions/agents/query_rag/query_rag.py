7b3e7ebc7f5e9ead762bca127ba920484f874805
Fahad <FFaruqi@dot.nyc.gov>
2024-05-09 15:29:35 -0400

    adding starter langchain elements

--- langchain_functions/agents/query_rag/query_rag.py
+++ langchain_functions/agents/query_rag/query_rag.py
@@ -1,1 +1,16 @@
 # query the FAISS database
+from langchain_functions.vector-store.similarity_serch import query
+from langchain_community.vector-store.add_data import *
+import asyncio
+
+async def query_db(query_str: str):
+    return await query(query_str)
+
+async def add_to_db(data: any):
+    pass
+
+if __name__ == "__main__":
+    async def main():
+        print(query_db("test"))
+
+    asyncio.run(main())
--- langchain_functions/vector-store/similarity_search.py
+++ langchain_functions/vector-store/similarity_search.py
@@ -2,18 +2,17 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-if OPENAI_API_KEY is None:
-    raise Exception("Missing OPENAI_API_KEY")
+async def query(query: str):
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    if OPENAI_API_KEY is None:
+        raise Exception("Missing OPENAI_API_KEY")
 
-db = FAISS.load_local(
-    "faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True
-)
+    db = FAISS.load_local(
+        "faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True
+    )
 
-query = "what am i gonna do?"
-docs = db.similarity_search(query)
+    docs = await db.similarity_search(query)
 
-print(docs[0].page_content)
+    return docs
