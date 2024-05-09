3c95f366b5a0692206fef5b7873edfce49c9bd49
BVasquez07 <bvasq07@gmail.com>
2024-05-09 17:33:43 -0400

    pushing updates for checkout

--- langchain_functions/agents/internetSearch/internet.py
+++ langchain_functions/agents/internetSearch/internet.py
@@ -21,8 +21,7 @@ BING_API_KEY = os.getenv('BING_API_KEY')
 
 """
 TO DO: get request on the sources and use beautiful soup to extract the text and relevant links within the page(look at previous commits for that). returned by the query and prep them to be sent to the DB (make a function for that and put it in another file)
-TO DO: implement DB interaction.
-https://beautiful-soup-4.readthedocs.io/en/latest/
+TO DO: generate file which will be used to populate DB
 
 """
 
