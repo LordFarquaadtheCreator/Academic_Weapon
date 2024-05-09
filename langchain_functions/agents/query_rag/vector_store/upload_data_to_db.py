78f38ed44eaac49b2d072b13563046f8b61d70c1
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-09 16:12:56 -0400

    removed unused imports

--- langchain_functions/agents/query_rag/vector_store/upload_data_to_db.py
+++ langchain_functions/agents/query_rag/vector_store/upload_data_to_db.py
@@ -1,8 +1,5 @@
-from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 import os
 
