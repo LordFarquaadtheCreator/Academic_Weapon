2f01fe54d43cc841bf5524dbd8a5f473d7802b58
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-10 14:37:47 -0400

    final version of server for demo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # doc: https://pgjones.gitlab.io/quart/reference/cheatsheet.html#cheatsheet
-from quart import Quart, request
-from langchain_functions import base_agent
+from quart import Quart, Response, request, jsonify
+from langchain_functions.base_agent import get_answer, get_context
 
 app = Quart(__name__)
 
@@ -9,11 +9,19 @@ app = Quart(__name__)
 def hello():
     return "wrong endpoint"
 
-@app.route("/query", methods=["GET"])
+
+@app.route("/query")
 async def query():
-    query = request.args.get("query")
-    res = await base_agent(query, 1)
-    return res
+    query = request.args.get("query", default="", type=str)
+    if not query:
+        return jsonify({"error": "Query parameter is required"}), 400
+
+    context_count = request.args.get("context_count", default=5, type=int)
+
+    response = await get_answer(query, context_count)
+
+    return response.content
+
 
 if __name__ == "__main__":
     app.run(debug=True)
