2b60c3cb525e2810ebc43947914cbee48180c29a
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-10 14:56:45 -0400

    added input usestate and axios get request to ui

--- UI/src/PromptInput.jsx
+++ UI/src/PromptInput.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PromptInput.css';
+import axios from 'axios';
 
 function PromptInput() {
+    const [input, setInput] = useState('');
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.get("http://127.0.0.1:5000/query", {
+            params: {
+                query: input
+            }
+        }).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="prompt-input">
-            <textarea placeholder="Enter Prompt Here"></textarea>
+            <textarea id="userinput" onChange={handleChange} placeholder="Enter Prompt Here"></textarea>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 # doc: https://pgjones.gitlab.io/quart/reference/cheatsheet.html#cheatsheet
 from quart import Quart, request
 from langchain_functions import base_agent
+from quart_cors import cors
 
-app = Quart(__name__)
+app = cors(Quart(__name__), allow_origin="http://localhost:5173/")
 
 
 @app.route("/")
 def hello():
     return "wrong endpoint"
 
+
 @app.route("/query", methods=["GET"])
 async def query():
     query = request.args.get("query")
     res = await base_agent(query, 1)
     return res
+
 
 if __name__ == "__main__":
     app.run(debug=True)
