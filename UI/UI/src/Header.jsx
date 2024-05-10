d29a98385aeacef70d7804a277e4e22d12d7e159
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-10 14:40:26 -0400

    fixed path issue with logo

--- UI/UI/src/Header.jsx
+++ UI/UI/src/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logo from './assets/academicBEAST.webp'; 
+import logo from '/assets/academicBEAST.webp'; 
 function Header() {
     return (
         <header className="header">
