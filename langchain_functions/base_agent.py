0dd1fb0da4180b539811c7058914ba8b44aca70b
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-09 22:18:43 -0400

    added error handling

--- langchain_functions/base_agent.py
+++ langchain_functions/base_agent.py
@@ -1,10 +1,10 @@
 from agents.query_rag.query_rag import query_db, add_to_db
 from agents.internetSearch.internet import searchOrNot
-from functions import needBing
+from functions.needBing import need_bing
 
 async def langchain(query: str, num_results: int, save = False):
     results = []
-    
+
     # check database
     try:
         results = query_db(query, num_results)
@@ -12,14 +12,16 @@ async def langchain(query: str, num_results: int, save = False):
         print(e)
 
     # calculate overall confidence
-    confidence: int = sum(results[1])
-    if(needBing(confidence)):
-        # search on the internet
-        try:
-            results.append(await searchOrNot(query))
-        except Exception as e:
-            print(e)
+    confidence: int = sum(results[1])/len(results[1])
+    needs_bing = need_bing(confidence)
 
+    if(not needs_bing):
+        try:
+            res = searchOrNot(query)
+            print("res", res)
+            results.append(res)
+        except Exception as e:
+            print("Exception Occured",  e)
 
     if save:
         try:
@@ -27,13 +29,14 @@ async def langchain(query: str, num_results: int, save = False):
         except Exception as e:
             print(e)
 
-    return results
+    return results[0]
 
 
 if __name__ == "__main__":
     import asyncio 
 
     async def main():
-        print(await langchain("im gonna  ", 4))
+        res = await langchain("the mitocondria is the powerhouse of the cell  ", 4)
+        print(res)
     
     asyncio.run(main())
