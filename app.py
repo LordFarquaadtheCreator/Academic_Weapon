1ad1552ac4b9670baa545461098d1ba79e75fc58
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-24 10:49:47 -0400

    misc whitespace

--- app.py
+++ app.py
@@ -16,16 +16,17 @@ async def query():
     context = int(request.args.get("content")) or 5
     res = await get_response(query=query, context_count=context)
     #'>' not supported between instances of 'str' and 'int'
-    #this error is caused by the context being a string and not an integer in previous res = await get_response(query, str(context))
+    # this error is caused by the context being a string and not an integer in previous res = await get_response(query, str(context))
     return str(res)
 
+
 # https://stackoverflow.com/questions/70138056/json-server-access-to-localhost-was-denied-in-chrome
-#possible reason why this port keeps disconneting 
-#is because of the cors policy and the airplay on port 5000 for apple devices
-#however the issue is definetly apparent if the 
-#if you are having issues with access to localhost being denied visit chrome://net-internals/#sockets and flush the sockets.
-#last alternative would be to switch the port.
-#basically DO NOT START THE VITE SERVER BEFORE THE QUART SERVER.
+# possible reason why this port keeps disconneting
+# is because of the cors policy and the airplay on port 5000 for apple devices
+# however the issue is definetly apparent if the
+# if you are having issues with access to localhost being denied visit chrome://net-internals/#sockets and flush the sockets.
+# last alternative would be to switch the port.
+# basically DO NOT START THE VITE SERVER BEFORE THE QUART SERVER.
 
 if __name__ == "__main__":
     app.run(debug=True)
