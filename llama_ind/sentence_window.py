58094bd71d5d40c5ce94ce917e8bae6d1d55803c
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-26 17:29:11 -0400

    sentence window retrival

---
+++ llama_ind/sentence_window.py
@@ -0,0 +1,33 @@
+from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Document
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+
+def init_defaults():
+    from llama_index.core import Settings
+
+    node_parser = SentenceWindowNodeParser.from_defaults(
+        window_size=3,
+        window_metadata_key="window",
+        original_text_metadata_key="original_text",
+    )
+
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+    text_splitter = SentenceSplitter()
+
+    Settings.text_splitter = text_splitter
+
+    return node_parser, text_splitter
+
+
+def sentence_window_setup(documents: Document) -> Document:
+    # from llama_index.core import VectorStoreIndex
+
+    node_parser, text_splitter = init_defaults()
+
+    nodes = node_parser.get_nodes_from_documents(documents)
+
+    # sentence_index = VectorStoreIndex(nodes)
+    return nodes
