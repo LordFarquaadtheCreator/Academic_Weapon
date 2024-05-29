d92f4d943abc12056b58ccf4fccd4b99ff01392d
BVasquez07 <bvasq07@gmail.com>
2024-05-29 13:02:25 -0400

    loading animation for input submission

--- interface/src/PromptInput.jsx
+++ interface/src/PromptInput.jsx
@@ -13,7 +13,7 @@ function PromptInput() {
         setInput(e.target.value);
     }
     const handleSubmitClick = () => {
-        console.log("Submit button clicked")
+        setLoading(prevState => !prevState);
         setSubmitingData(prevState => !prevState);
     }
 
@@ -27,8 +27,10 @@ function PromptInput() {
                     content: 5
                 }
             }).then((response) => {
+                setLoading(prevState => !prevState);
                 setData(response.data)
             }).catch((error) => {
+                setLoading(prevState => !prevState);
                 if(error.response){
                     setData(`Request passed successfully, however there was an error with the server and responded with status: ${error.response.status}`)
                 }
@@ -54,13 +56,18 @@ function PromptInput() {
                         style={{width: 700, height: 55}} 
                         rows="2" 
                         className="resize rounded-md block mx-auto p-2.5 text-lg text-gray-500 rounded-lg border border-perfectGrey focus:ring-blue-500 focus:border-deepWhite bg-perfectGrey border-gray-600 placeholder-gray-300 text-white focus:ring-blue-500 focus:border-blue-500 outline-none" 
-                        placeholder="Input your question here...">
+                        placeholder="Input your question here..."> 
                     </textarea>
 
                     <button className="inline-flex justify-center p-2 rounded-full cursor-pointer text-lg text-deepWhite hover:bg-mutedRed" onClick={handleSubmitClick}>
-                        <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                            <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
-                        </svg>
+                        {!loading?
+                            <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
+                            </svg>
+                        :   <svg className="animate-spin h-6 w-5 mr-3"  viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="3"></circle>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>}
                     </button>
                 </div>
                 {data ? <ResponseField responseData={data} /> : ""}
