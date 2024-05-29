fe092a97c31a2fa24e5c852a309f7bb1da7681b4
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-29 18:26:17 -0400

    adding window extractor

--- llama_ind/app.py
+++ llama_ind/app.py
@@ -3,29 +3,37 @@ def main():
     from llama_ind.init_db import create_db
     from llama_index.core.postprocessor import MetadataReplacementPostProcessor
     from llama_ind.query_rewrite import generate_queries
+    from llama_ind.get_window import get_window
+
+    # query rewriter -> n-queries
+    # n-queries -> database for #m results
+    # rerank
+    # prompt
 
     try:
-        # res = generate_queries(
-        #     "What is a derivative??"
-        # )
-        res = "What is a derivative?"
+        res = generate_queries("What is a derivative??")
+        # exit()
+
         # index = create_db()
         index = get_db_index()
-        query_engine = index.as_query_engine(
+        query_engine = index.as_retriever(
             similarity_top_k=1,
             node_postprocessors=[
                 MetadataReplacementPostProcessor(target_metadata_key="window"),
             ],
-            streaming=True
         )
-        # streaming output:
-        # https://docs.llamaindex.ai/en/stable/module_guides/deploying/query_engine/streaming/
-        streaming_response = query_engine.query(res)
 
-        response = ""
-        for re in streaming_response.response_gen:
-            response += re
-            print(response)
+        # extract window
+        data = []
+        for question in res:
+            query_response = query_engine.retrieve(question)
+            data.append(get_window(query_response))
+
+        print(data)
+        # response = ""
+        # for re in streaming_response.response_gen:
+        #     response += re
+        #     print(response)
 
     except Exception as e:
         print(e)
---
+++ llama_ind/get_window.py
@@ -0,0 +1,3 @@
+def get_window(obj):
+    meta = obj[0].metadata
+    return meta.get("window")
