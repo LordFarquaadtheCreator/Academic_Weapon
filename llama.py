4c34a43eb32ab301761cbe934495defcc3513b44
fahad <fahadfaruqi1@gmail.com>
2024-05-29 23:37:43 -0400

    misc

--- llama.py
+++ llama.py
@@ -1,4 +1,4 @@
 from llama_ind.app import main
 
 if __name__ == "__main__":
-    main(query="man what even is linear algebra, what's an eigenvector??")
+    main(query="what are the main subjects of calculus?")
