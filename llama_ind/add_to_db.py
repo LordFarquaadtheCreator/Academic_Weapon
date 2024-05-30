d244a9cf373b9ed75f9520bb23bb24ca6c087ce4
fahad <fahadfaruqi1@gmail.com>
2024-05-30 00:18:19 -0400

    updated add_to_db.py - works & persists

--- llama_ind/add_to_db.py
+++ llama_ind/add_to_db.py
@@ -5,12 +5,14 @@ from llama_ind.timer import timer
 def add_to_db(db, path: str) -> None:
     try:
         from llama_index.core import SimpleDirectoryReader
+        # from llama_index import StorageContext
         from llama_ind.sentence_window import sentence_window_setup
 
         documents = SimpleDirectoryReader(path).load_data()
         nodes = sentence_window_setup(documents)
 
-        db._add_nodes_to_index(nodes)
+        db.insert_nodes(nodes)
+        db.storage_context.persist(persist_dir="db")
 
     except Exception as e:
         print("hellooooo error on add to db")
